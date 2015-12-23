18bdc723d55714251919f549549050af5bd434ef
s5050471 <s5050471@kmitl.ac.th>
2015-12-23 16:43:23 +0700

    update UI

--- src/First_Page.java
+++ src/First_Page.java
@@ -7,10 +7,16 @@ import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import swing2swt.layout.BoxLayout;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.TrayItem;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.widgets.ProgressBar;
 
 public class First_Page {
 
-	protected Shell shell;
+	protected Shell shlLiveGoon;
 
 	/**
 	 * Launch the application.
@@ -31,9 +37,9 @@ public class First_Page {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlLiveGoon.open();
+		shlLiveGoon.layout();
+		while (!shlLiveGoon.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -44,12 +50,13 @@ public class First_Page {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(800, 600);
-		shell.setText("SWT Application");
+		shlLiveGoon = new Shell();
+		shlLiveGoon.setSize(800, 600);
+		shlLiveGoon.setText("LIVE GoOn");
+		shlLiveGoon.setLayout(null);
 		
-		Menu menu = new Menu(shell, SWT.BAR);
-		shell.setMenuBar(menu);
+		Menu menu = new Menu(shlLiveGoon, SWT.BAR);
+		shlLiveGoon.setMenuBar(menu);
 		
 		MenuItem mntmFile = new MenuItem(menu, SWT.CASCADE);
 		mntmFile.setText("File");
@@ -69,8 +76,32 @@ public class First_Page {
 		MenuItem mntmView = new MenuItem(menu, SWT.NONE);
 		mntmView.setText("View");
 		
+		MenuItem mntmSettig = new MenuItem(menu, SWT.CASCADE);
+		mntmSettig.setText("Option");
+		
+		Menu menu_2 = new Menu(mntmSettig);
+		mntmSettig.setMenu(menu_2);
+		
+		MenuItem mntmOption = new MenuItem(menu_2, SWT.NONE);
+		mntmOption.setText("Setting");
+		
 		MenuItem mntmHelp = new MenuItem(menu, SWT.NONE);
 		mntmHelp.setText("Help");
+		
+		Label label = new Label(shlLiveGoon, SWT.SEPARATOR | SWT.VERTICAL);
+		label.setBounds(176, 10, 2, 521);
+		
+		Button btnNewButton = new Button(shlLiveGoon, SWT.NONE);
+		btnNewButton.setBounds(43, 35, 75, 25);
+		btnNewButton.setText("LIVE");
+		
+		Button btnNewButton_1 = new Button(shlLiveGoon, SWT.NONE);
+		btnNewButton_1.setBounds(43, 86, 75, 25);
+		btnNewButton_1.setText("Upload");
+		
+		Button btnNewButton_2 = new Button(shlLiveGoon, SWT.NONE);
+		btnNewButton_2.setBounds(43, 141, 75, 25);
+		btnNewButton_2.setText("Media File");
 
 	}
 }
