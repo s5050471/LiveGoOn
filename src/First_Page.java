0ba32fffbe352253fd48bbf7be77367245e593a6
s5050471 <s5050471@kmitl.ac.th>
2016-01-01 20:47:39 +0700

    Update 2 manu

--- src/First_Page.java
+++ src/First_Page.java
@@ -17,10 +17,20 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import java.awt.Frame;
 import org.eclipse.swt.awt.SWT_AWT;
+import java.awt.Panel;
+import java.awt.BorderLayout;
+import javax.swing.JRootPane;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
-public class First_Page {
+public class First_Page extends Thread{
 
 	protected Shell shlLiveGoon;
+	private Table table;
 	
 
 	/**
@@ -51,6 +61,7 @@ public class First_Page {
 				  System.exit(0);
 		      }
 		});
+		//new First_Page().start();
 		afterLoad();
 		while (!shlLiveGoon.isDisposed()) {
 			if (!display.readAndDispatch()) {
@@ -117,19 +128,116 @@ public class First_Page {
 		Button btnNewButton_2 = new Button(shlLiveGoon, SWT.NONE);
 		btnNewButton_2.setBounds(43, 141, 75, 25);
 		btnNewButton_2.setText("Media File");
+		
+		composite_1 = new Composite(shlLiveGoon, SWT.EMBEDDED);
+		composite_1.setBounds(184, 10, 590, 521);
+		composite_1.setLayout(null);
+		composite_1.setVisible(false);
+		
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				composite.setVisible(true);
+				composite_1.setVisible(false);
+				composite_2.setVisible(false);
+			}
+		});
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				composite.setVisible(false);
+				composite_1.setVisible(true);
+				composite_2.setVisible(false);
+			}
+		});
+		
+		btnNewButton_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				composite.setVisible(false);
+				composite_1.setVisible(false);
+				composite_2.setVisible(true);
+			}
+		});
+		
+		table = new Table(composite_1, SWT.BORDER | SWT.FULL_SELECTION);
+		table.setBounds(0, 0, 590, 291);
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn.setWidth(40);
+		tblclmnNewColumn.setText("No.");
+		
+		TableColumn tblclmnNewColumn_1 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_1.setWidth(100);
+		tblclmnNewColumn_1.setText("Name");
+		
+		TableColumn tblclmnSize = new TableColumn(table, SWT.NONE);
+		tblclmnSize.setWidth(100);
+		tblclmnSize.setText("Size");
+		
+		TableColumn tblclmnProgress = new TableColumn(table, SWT.NONE);
+		tblclmnProgress.setWidth(100);
+		tblclmnProgress.setText("Progress");
+		
+		TableColumn tblclmnNewColumn_2 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_2.setWidth(100);
+		tblclmnNewColumn_2.setText("Total Upload");
+		
+		TableColumn tblclmnUploadSpeed = new TableColumn(table, SWT.NONE);
+		tblclmnUploadSpeed.setWidth(100);
+		tblclmnUploadSpeed.setText("Upload Speed");
+		
+		TabFolder tabFolder = new TabFolder(composite_1, SWT.NONE);
+		tabFolder.setBounds(0, 306, 590, 215);
+		
+		TabItem tbtmNewItem = new TabItem(tabFolder, SWT.NONE);
+		tbtmNewItem.setText("Summary");
+		
+		TabItem tbtmNewItem_1 = new TabItem(tabFolder, SWT.NONE);
+		tbtmNewItem_1.setText("Statistics");
+		
+		composite_2 = new Composite(shlLiveGoon, SWT.EMBEDDED);
+		composite_2.setBounds(184, 10, 590, 521);
+		composite_2.setVisible(false);
+		
+		table_1 = new Table(composite_2, SWT.BORDER | SWT.FULL_SELECTION);
+		table_1.setBounds(0, 0, 598, 521);
+		table_1.setHeaderVisible(true);
+		table_1.setLinesVisible(true);
+		
+		TableColumn tbl1clmnNewColumn = new TableColumn(table_1, SWT.NONE);
+		tbl1clmnNewColumn.setWidth(100);
+		tbl1clmnNewColumn.setText("Name");
+		
+		TableColumn tbl1clmnSize = new TableColumn(table_1, SWT.NONE);
+		tbl1clmnSize.setWidth(100);
+		tbl1clmnSize.setText("Size");
+		
+		TableColumn tbl1clmnNewColumn_1 = new TableColumn(table_1, SWT.NONE);
+		tbl1clmnNewColumn_1.setWidth(100);
+		tbl1clmnNewColumn_1.setText("Time");
+		
+		TableColumn tbl1clmnDate = new TableColumn(table_1, SWT.NONE);
+		tbl1clmnDate.setWidth(100);
+		tbl1clmnDate.setText("Date");
+		
 
 	}
+	Composite composite,composite_1,composite_2;
+	private Table table_1;
 	protected void afterLoad(){
 		Webcam webcam = Webcam.getDefault();
 		webcam.setViewSize(WebcamResolution.VGA.getSize());
 		webcam.open();
-
+		
 		WebcamPanel cam = new WebcamPanel(webcam);
 		cam.setBounds(0, 0, 590, 523);
 		cam.setFPSDisplayed(true);
 		cam.setMirrored(true);
 
-		Composite composite = new Composite(shlLiveGoon, SWT.EMBEDDED);
+		composite = new Composite(shlLiveGoon, SWT.EMBEDDED);
 		composite.setBounds(184, 10, 590, 521);
 		
 		Frame frame = SWT_AWT.new_Frame(composite);
@@ -138,5 +246,9 @@ public class First_Page {
 		frame.add(cam);
 		frame.pack();
 		frame.setVisible(true);
+	}
+	@Override
+	public void run(){
+		afterLoad();
 	}
 }
